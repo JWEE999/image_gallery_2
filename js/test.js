e915a1432233dbb5dd421f17f35bb817d2c734b5
JWEE999 <164032410+JWEE999@users.noreply.github.com>
2024-04-13 22:43:21 +0800

    Update test.js

--- js/test.js
+++ js/test.js
@@ -361,11 +361,11 @@ function endTouch(e) {
   }
 
   function moveTouch(e){
-      e.preventDefault()
-	  if (in_preview_mode===true){
-		current_X_pos = e.touches[0].clientX - initialX;
-		document.getElementById("mySlides_"+slideIndex).style.setProperty('left', current_X_pos +"px");
-	  }
+      e.preventDefault();
+	  //if (in_preview_mode===true){
+		//current_X_pos = e.touches[0].clientX - initialX;
+		//document.getElementById("mySlides_"+slideIndex).style.setProperty('left', current_X_pos +"px");
+	  //}
   }
   
   function plusslide(){
